c7ab8e46658c4e9a5bea7482ceb0b20903eaf302
pradnya-desai <pradnya.d10@gmail.com>
2022-02-01 09:57:16 -0800

    login integration code added

--- src/adminLogin/Sidebar.js
+++ src/adminLogin/Sidebar.js
@@ -8,43 +8,43 @@ const Sidebar = () => {
    
         <div>
 
-<Nav variant="pills" activeKey="1">
+<Nav variant="pills" activeKey="1" >
       <Nav.Item>
         <Nav.Link eventKey="1">
        <NavLink to="/dashboard"> Dashboard</NavLink>
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="2" title="Item">
+        <Nav.Link eventKey="2" title="Item" style={{color:"black"}}>
           Respond To queries
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="3">
-        <NavLink to="/sales"> Sales/Profit</NavLink>
+        <NavLink to="/sales" style={{color:"black"}}> Sales/Profit</NavLink>
         </Nav.Link>
       </Nav.Item>
       <NavDropdown title="Cakes" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.1" style={{color:"black"}}>Action</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.2" style={{color:"black"}}>Another action</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.3" style={{color:"black"}}>Something else here</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.4" style={{color:"black"}}>Separated link</NavDropdown.Item>
       </NavDropdown>
-      <NavDropdown title="Orders" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+      <NavDropdown title="Orders" id="nav-dropdown" style={{color:"black"}}>
+        <NavDropdown.Item eventKey="4.1"style={{color:"black"}}>Action</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.2"style={{color:"black"}}>Another action</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.3" style={{color:"black"}}>Something else here</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.4" style={{color:"black"}}>Separated link</NavDropdown.Item>
       </NavDropdown>
       <Nav.Item>
         <Nav.Link eventKey="5">
-        <NavLink to='/changepassword'>Change Password</NavLink>
+        <NavLink to='/changepassword' style={{color:"black"}}>Change Password</NavLink>
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="5">
+        <Nav.Link style={{color:"black"}} eventKey="5">
         Logout
         </Nav.Link>
       </Nav.Item>
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,34 +2,76 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Navbarr from './Navbarr.js'
 import Sidebar from '../adminLogin/Sidebar.js'
+import { useState } from 'react'
 const Login = () => {
+const [admin,setAdmin]=useState({
+	  email:'', 
+	  password:'' 
 
+});
+
+let name, value;
+const handleInput = (e) => {
+	console.log(e);
+	  name = e.target.name;
+	  value = e.target.value;
+	  setAdmin({...admin,[name]:value});
+	  console.log(admin);
+	}
+
+
+const loginData= async(e)=>{
+e.preventDefault();
+const { email, password } = admin;
+
+const response = await fetch('/admin/login', {
+	  method: 'POST', 
+	  headers: {
+		"Content-Type": "application/json"
+},
+body:JSON.stringify({
+email, password
+    })
+    });
+	const result=await response.json();
+	if(result.status===403|| !result){
+		alert('Invalid Credentials');
+		
+	}
+	else{
+		alert('Login Successful');
+		window.location.href='/adminlogin';
+
+	}
+}
     return (
         <div>
             <div style={{marginTop:"-40px"}}>{Navbarr}</div>
              <div className="contact-item2" style={{paddingTop:"15%"}}>
              <h1 style={{backgroundColor:"#fecacc"}}>Admin Login</h1>
-
+<form method='post'>
         <div class="container" style={{marginLeft:"30%"}}>
 
 
 	<div class="row input-container" style={{display:"inline"}}>
 			<div class="col-xs-12">
             <div class="styled-input1">
-					<input type="text" className="contact-input" required />
+					<input type="text" value={admin.email} name='email' onChange={handleInput} className="contact-input" required />
 					<label>Email</label> 
 				</div>
 			</div>
 			
 			<div class="col-md-6 col-sm-12">
 				<div class="styled-input1" style={{float:"right"}}>
-					<input type="password"  className="contact-input" required />
+					<input type="password" value={admin.password} name='password' onChange={handleInput} className="contact-input" required />
 					<label>Password</label> 
 				</div>
 			</div>
        <br/>
 			<div class="col-md-6 col-sm-12">
-				<Link to='/adminlogin' target="_blank"><div class="btn-lrg submit-btn">Sign In</div></Link>
+				{/* <Link to='/adminlogin' target="_blank"> */}
+					<div class="btn-lrg submit-btn"><button onClick={loginData}>Sign In</button></div>
+					{/* </Link> */}
 			</div>
             <br/>
             <br/>
@@ -39,7 +81,7 @@ const Login = () => {
 	</div>
 </div>
 
-      
+      </form>
 
         </div>
     </div>
